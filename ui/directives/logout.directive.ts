77ce354382d1994d88f785e9ddff74c2af9b5aff
azandrew-sidoine <azandrewdevelopper@gmail.com>
2026-05-23 19:46:38 +0000

    updated logout directive

--- ui/directives/logout.directive.ts
+++ ui/directives/logout.directive.ts
@@ -3,11 +3,13 @@ import {
   EventEmitter,
   HostListener,
   Inject,
+  Input,
   Output,
 } from "@angular/core";
 import { AuthServiceInterface } from "../../types";
 import { AUTH_SERVICE } from "../../core";
 import { lastValueFrom } from "rxjs";
+import { Dialog, DIALOG } from "../../../directives/dialog";
 
 @Directive({
   standalone: true,
@@ -15,17 +17,29 @@ import { lastValueFrom } from "rxjs";
 })
 export class LogoutDirective {
   @HostListener("click") async onClick() {
+    this.doLogout();
+  }
+  @Input() prompt!: string;
+  @Input() revoke: boolean = true;
+  @Output("performing-action") performingAction = new EventEmitter<boolean>();
+
+  constructor(@Inject(AUTH_SERVICE) private auth: AuthServiceInterface, @Inject(DIALOG) private dialog: Dialog) { }
+
+  private async doLogout() {
+    if (this.prompt && this.dialog) {
+      const result = await this.dialog.confirm(this.prompt);
+      if (result === false) {
+        return;
+      }
+    }
+
     try {
       this.performingAction.emit(true);
-      await lastValueFrom(this.auth.signOut(true).pipe());
-      this.performingAction.emit(false);
+      await lastValueFrom(this.auth.signOut(this.revoke));
     } catch (error) {
-      console.error("Logout error: ", error);
+      console.error("[Logout] ", error);
+    } finally {
       this.performingAction.emit(false);
     }
   }
-
-  @Output("performing-action") performingAction = new EventEmitter<boolean>();
-
-  constructor(@Inject(AUTH_SERVICE) private auth: AuthServiceInterface) {}
 }
